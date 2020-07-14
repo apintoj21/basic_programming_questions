3839d259e542da70bda470035084b2dbff9ef6ef
apintoj21 <apinto_j21@yahoo.in>
2020-07-14 17:20:44 +0100

    q3

--- common_functions.py
+++ common_functions.py
@@ -1,4 +1,5 @@
 from random import randint
+from functools import reduce
 
 
 def integer_array(n):
@@ -13,3 +14,25 @@ def unsorted_array(a, b, l):
     for i in range(l + 1):
         u_a.append(randint(a, b))
     return u_a
+
+
+def list_duplicates_of(seq, item):
+    start_at = -1
+    indices = []
+    while True:
+        try:
+            index = seq.index(item, start_at + 1)
+        except ValueError:
+            break
+        else:
+            indices.append(index)
+            start_at = index
+    return indices
+
+
+def factorial(n):
+    return reduce(lambda x, y: x * y, range(1, n + 1), 1)
+
+
+def combinations(n, r):
+    return factorial(n) / (factorial(r) * factorial(n - r))
---
+++ p3.py
@@ -0,0 +1,31 @@
+"""How do you find all pairs of an integer array whose sum is equal to a given number?
+"""
+from common_functions import list_duplicates_of
+
+
+
+def pair_eq2_sum(array, pair_sum):
+    a_map = {}
+    # create the map
+    for i, e in enumerate(array):
+        a_map[e] = i
+    # lookup
+    for i, e in enumerate(array):
+        if pair_sum - e in a_map:
+            if e != pair_sum/2:
+                print("pair = {} and {}".format(array[i], array[a_map.get(pair_sum - e)]))
+
+    l1 = list_duplicates_of(array, pair_sum/2)
+    if len(l1) > 1:
+        for i in range(len(l1)):
+            print("pair = {} and {}".format(int(pair_sum/2), int(pair_sum/2)))
+
+
+def main():
+    arr = [3, 4, 5, 4, 5]
+
+    pair_eq2_sum(arr, 8)
+
+
+if __name__ == "__main__":
+    main()
